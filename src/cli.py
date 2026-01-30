7c925fb74cc5625501f0d4c59869624a2085ea08
DeyanM1 <de.mrkv@gmail.com>
2026-01-30 15:01:00 +0100

    added cli runner script

---
+++ src/cli.py
@@ -0,0 +1,30 @@
+import argparse
+from pathlib import Path
+import main
+from functions import ZFile
+
+
+
+def start():
+    parser = argparse.ArgumentParser(description="Run .zph files")
+    parser.add_argument("filepath", type=str, help="Path to the file")
+    args = parser.parse_args()
+
+    file_path = Path(args.filepath)
+    
+    # Validate file
+    if not file_path.exists() or not file_path.is_file():
+        print(f"Error: {file_path} does not exist or is not a file")
+        return
+    
+    zfile = ZFile(file_path)
+
+
+    ### RUN ###
+
+    main.lexer(zfile)
+    main.compile(zfile)
+
+
+if __name__ == "__main__":
+    start()
