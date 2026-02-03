bf1bb64ec62fbfc7c37af6abf4f9a60cd7630bbe
DeyanM1 <de.mrkv@gmail.com>
2026-02-03 13:51:47 +0100

    added build script and compiled to exe

---
+++ dist/build.bat
@@ -0,0 +1,26 @@
+@echo off
+
+uv run nuitka src/zcli.py --onefile --output-dir=dist/windows --output-filename=zcli.exe --assume-yes-for-downloads
+
+REM uv run nuitka src/zcli.py --onefile --output-dir=dist/linux --output-filename=zlm
+
+REM uv run nuitka src/zlm.py --onefile --output-dir=dist/windows --output-filename=zlm.exe --assume-yes-for-downloads
+
+REM uv run nuitka src/zlm.py --onefile --output-dir=dist/linux --output-filename=zlm 
+
+
+cd .\dist\windows
+rmdir /s /q zcli.build
+rmdir /s /q zcli.dist
+rmdir /s /q zcli.onefile-build
+rmdir /s /q zlm.build
+rmdir /s /q zlm.dist
+rmdir /s /q zlm.onefile-build
+
+cd ..\linux
+rmdir /s /q zcli.build
+rmdir /s /q zcli.dist
+rmdir /s /q zcli.onefile-build
+rmdir /s /q zlm.build
+rmdir /s /q zlm.dist
+rmdir /s /q zlm.onefile-build
---
+++ dist/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+uv run nuitka src/zcli.py --onefile --output-dir=dist/windows --output-filename=zlm.exe --assume-yes-for-downloads
+
+uv run nuitka src/zcli.py --onefile --output-dir=dist/linux --output-filename=zlm
+
+uv run nuitka src/zlm.py --onefile --output-dir=dist/windows --output-filename=zlm.exe --assume-yes-for-downloads
+
+uv run nuitka src/zlm.py --onefile --output-dir=dist/linux --output-filename=zlm 
+
+
+cd ./dist/windows
+rm -rf zcli.build zcli.dist zcli.onefile-build zlm.build zlm.dist zlm.onefile-build
+
+cd ../linux/
+rm -rf zcli.build zcli.dist zcli.onefile-build zlm.build zlm.dist zlm.onefile-build
